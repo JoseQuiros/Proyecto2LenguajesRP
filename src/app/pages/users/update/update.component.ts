e0c30f034ff5c2ec88f61bb833d2aad6b2e524a9
JoseQuiros <jqm.25@hotmail.es>
2022-07-22 12:51:17 -0600

    update user component,it is necessary to conclude

--- src/app/pages/users/update/update.component.ts
+++ src/app/pages/users/update/update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Users, UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-update',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
-
-  constructor() { }
+  id:string="";
+  userUpdated: Users={
+    iduser: '', 
+    idrol:'',
+    name: '', 
+    dni: '',
+    age: '',
+    telephone: '',
+    email: '',
+    clave: '',
+    state: ''
+  };
+  constructor(private userService:UsersService,private antivateRouter: ActivatedRoute,
+    private router:Router) { }
 
   ngOnInit(): void {
+    this.id= this.antivateRouter.snapshot.params.id;
+    this.userService.getUsersById(this.id).subscribe(
+      res=>{
+        this.userUpdated=res;
+      },
+
+      err=>console.log(err)
+    );
+  }
+  updateUser(){
+    this.userService.editUser(this.id, this.userUpdated).subscribe(
+      res=>{
+        this.router.navigate(['/list']);
+      },
+
+      err=>console.log(err)
+    );
   }
 
 }
